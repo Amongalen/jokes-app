4abccdad18370bd42d9391db7e953c13d5021bee
Adam Parys <aparys@ekokal.com>
2021-01-30 11:00:14 +0100

    Created basic domain POJOs.

---
+++ src/main/java/com/amongalen/jokesapp/domain/Joke.java
@@ -0,0 +1,20 @@
+package com.amongalen.jokesapp.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
+public class Joke {
+
+    String id;
+    JokeType type;
+    String setup;
+    String punchline;
+}
---
+++ src/main/java/com/amongalen/jokesapp/domain/JokeType.java
@@ -0,0 +1,17 @@
+package com.amongalen.jokesapp.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document
+public class JokeType {
+    String id;
+    String name;
+}
